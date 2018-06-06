9c379ad6563af9353ba9091e70422b6c22c4e052
Eric Andren <ericrandren@gmail.com>
2018-06-05 23:02:22 -0700

    RSEPC

--- page-contact.php
+++ page-contact.php
@@ -1,12 +1,10 @@
 <?php get_header(); ?>
 	<?php if (have_posts()): while (have_posts()) : the_post(); ?>
 		<main role="main">
-			<section>
-				<h1>
-					Ten years of experience is coupled with a hungry digital drive.
-				</h1>
-				<?php the_content(); ?>
-			</section>
+			<h1>
+				Ten years of experience is coupled with a hungry digital drive.
+			</h1>
+			<?php the_content(); ?>
 		</main>
 	<?php endwhile; endif; ?>
 <?php get_footer(); ?>
